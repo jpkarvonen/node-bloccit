7fedbec979f7750e388c67bb7f526259db6fcb46
John Karvonen <jpkarvonen@gmail.com>
2019-03-04 12:58:57 -0500

    add about page

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,10 +6,18 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200 and have 'Welcome Bloccit' in the body of the response", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("Welcome Bloccit");
+        expect(body).toContain("Welcome to Bloccit");
+        done();
+      });
+    });
+
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+      request.get(`${base}about`, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
         done();
       });
     });
