8f9d9ff5cffdc14cafa23fb19ae06f81aff41d16
John Karvonen <jpkarvonen@gmail.com>
2019-04-03 11:51:00 -0400

    add favorite scope for user profile page

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -2,6 +2,7 @@ const User = require("./models").User;
 const bcrypt = require("bcryptjs");
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require("./models").Favorite;
 
 module.exports = {
   createUser(newUser, callback){
@@ -34,10 +35,14 @@ module.exports = {
           Comment.scope({method: ["lastFiveFor", id]}).all()
           .then((comments) => {
             result["comments"] = comments;
-            callback(null, result);
-          })
-          .catch((err) => {
-            callback(err);
+            Favorite.scope({method: ["favoritePosts", id]}).all()
+            .then((favorites) => {
+              result["favorites"] = favorites;
+              callback(null, result);
+            })
+            .catch((err) => {
+              callback(err);
+            })
           })
         })
       }
